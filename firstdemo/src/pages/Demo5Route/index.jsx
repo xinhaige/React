0d45ccd6c0a1a5f343ea845561bfa5433ca6711c
1796827448 <1796827448@qq.com>
2022-08-26 14:44:37 +0800

    路由，很多东西没懂

--- firstdemo/src/pages/Demo5Route/index.jsx
+++ firstdemo/src/pages/Demo5Route/index.jsx
@@ -1,24 +1,32 @@
 import React, { Component } from 'react'
-import {Link,HashRouter,Route,Routes} from 'react-router-dom' //路由
+import {Link,HashRouter,Route,Routes,useParams,useNavigate,Navigate} from 'react-router-dom' //路由
 import Home from '../Home'
 import About from '../About'
 import './index.css'
 
 export default class index extends Component {
+
   render() {
     return (
-     <div>
-       <h2>a连接</h2>
-      <Link to={"/home/wode"} >Home</Link>
-         <Link to="/about" >About</Link> 
-       <div>
-        <h2>页面</h2>
-        <Routes>
-        <Route path='/home' element={<Home/>}  />
-        <Route path='/about' element={<About/>}/>
-        </Routes>
-       </div>
-     </div>
-    )
+      <div>
+        <h2>a连接</h2>
+       <Link to="/home/5">Home</Link>
+          <Link to={{pathname:"/about",state:{id:8,name:"wode"}}} >About</Link> 
+        <div>
+         <h2>页面</h2>
+         <Routes>
+          <Route path='/home/:id' element={<Home/>}  />
+          <Route path='/about' element={<About/>}/>
+         </Routes>
+         <button onClick={this.btnClick}>跳转</button>
+        </div>
+      </div>
+     )
+  }
+  btnClick=()=>{
+    return <Navigate to="/about" />;
+  }
+  componentDidMount(){
+    
   }
 }
--- firstdemo/src/pages/Home/index.jsx
+++ firstdemo/src/pages/Home/index.jsx
@@ -1,17 +1,38 @@
-import React, { Component } from 'react'
-
+import React, { Component,useEffect } from 'react'
+import {useParams} from 'react-router-dom'
 import './index.css'
 
-export default class index extends Component {
-  render() {
-    return (
-      <div>
-         <h1>这是home页面</h1>
-      </div>
-    )
-  }
-  componentDidMount(){
-   
+export default function Home(){
+   let  params= useParams();
   
-  }
+   useEffect(() => {
+    console.log("params",params); // {id: '123'}
+
+  }, []);
+
+   return (
+    <div>
+    <h1>这是home页面</h1>
+    </div>
+   )
 }
+
+
+
+// export default class index extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div>
+//       <h1>这是home页面</h1>
+//    </div>
+//      )
+//   }
+//   componentDidMount(){
+//     debugger
+//     let par=this.props;
+//     console.log(par);
+//   }
+// }
