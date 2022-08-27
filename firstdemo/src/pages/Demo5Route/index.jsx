07bf43379d63a5aaa6fd975b1a1928abaab8d2f7
1796827448 <1796827448@qq.com>
2022-08-27 11:30:14 +0800

    page/demo5 懒加载

--- firstdemo/src/pages/Demo5Route/index.jsx
+++ firstdemo/src/pages/Demo5Route/index.jsx
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component,lazy,Suspense} from 'react'
 import {Link,HashRouter,Route,Routes,useParams,useNavigate,Navigate} from 'react-router-dom' //路由
-import Home from '../Home'
-import About from '../About'
+// import Home from '../Home'
+// import About from '../About'
 import './index.css'
+
+const Home=lazy(()=>import("../Home"))  //懒加载 配合Suspense 使用
+const About=lazy(()=>import("../About")) //懒加载
 
 export default class index extends Component {
 
@@ -14,10 +17,12 @@ export default class index extends Component {
           <Link to={{pathname:"/about",state:{id:8,name:"wode"}}} >About</Link> 
         <div>
          <h2>页面</h2>
-         <Routes>
-          <Route path='/home/:id' element={<Home/>}  />
-          <Route path='/about' element={<About/>}/>
-         </Routes>
+         <Suspense fallback={<h1>loading</h1>}>
+            <Routes>
+             <Route path='/home/:id' element={<Home/>}  />
+             <Route path='/about' element={<About/>}/>
+            </Routes>
+         </Suspense>
          <button onClick={this.btnClick}>跳转</button>
         </div>
       </div>
