99683b5b29e4a9504ea6df5f806f48e7d7cf9095
1796827448 <1796827448@qq.com>
2022-08-27 08:48:53 +0800

    模态窗_闭包

--- firstdemo/src/Redux/count_reducer.js
+++ firstdemo/src/Redux/count_reducer.js
@@ -1,6 +1,8 @@
 //正常是 store.dispatch({type,num}) 执行下面的方法
 //如果传入的不是objec对象，而是函数（定时函数、异步函数）  则store挂在这个函数并将dispatch 传入函数中，在用传入的dispatch执行键值对obj
 
+
+//这里的函数只能是存函数 
 const state=0
 function reducerFun(preState=state,data){
     let {type,num}=data;
